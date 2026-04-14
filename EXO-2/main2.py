f52014a9be4f77529858306d85ccbe33d90c1d3b
Izayoii <rakhisahmat3600@gmail.com>
2026-04-14 12:21:12 +0200

    feat add banq account class

--- EXO-2/main2.py
+++ EXO-2/main2.py
@@ -4,16 +4,16 @@ compte1 = CompteBancaire("Alice Dupont", 1000.0, 200.0)
 compte2 = CompteBancaire("Bob Martin", 500.0)
 
 compte1.deposer(200.0)
-print(f"Solde d'Alice après dépôt : {compte1.solde}€")
+print(f"Solde d'Alice après dépôt est : {compte1.solde}€")
 
 compte1.retirer(150.0)
-print(f"Solde d'Alice après retrait : {compte1.solde}€")
+print(f"Solde d'Alice après retrait est : {compte1.solde}€")
 
 compte1.retirer(1200.0)  
 
 compte1.virement(compte2, 300.0)
-print(f"Solde d'Alice après virement : {compte1.solde}€")
-print(f"Solde de Bob après virement : {compte2.solde}€")
+print(f"Solde d'Alice après virement est : {compte1.solde}€")
+print(f"Solde de Bob après virement est : {compte2.solde}€")
 
 compte1.appliquer_interets()
 compte2.appliquer_interets()
