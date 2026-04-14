d6775ebc1c143202f21edf63581281ddf41abc14
Izayoii <rakhisahmat3600@gmail.com>
2026-04-14 10:14:42 +0200

    feat create car class

---
+++ EXO-1/voiture.py
@@ -0,0 +1,27 @@
+class Voiture:
+    nb_voitures = 0
+
+    def __init__(self, marque, modele, annee, kilometrage, prix_neuf):
+        self.marque = marque
+        self.modele = modele
+        self.annee = annee
+        self.kilometrage = kilometrage
+        self.prix_neuf = prix_neuf
+        Voiture.nb_voitures += 1
+
+    def afficher(self):
+        print(f"Marque: {self.marque}")
+        print(f"Modèle: {self.modele}")
+        print(f"Année: {self.annee}")
+        print(f"Kilométrage: {self.kilometrage} km")
+        print(f"Prix neuf: {self.prix_neuf} €")
+
+    def est_recente(self):
+        return self.annee >= 2020
+
+    def parcourir(self, distance):
+        self.kilometrage += distance
+
+    def estimer_valeur(self):
+        valeur = self.prix_neuf - 0.05 * self.kilometrage
+        return max(valeur, 500)
