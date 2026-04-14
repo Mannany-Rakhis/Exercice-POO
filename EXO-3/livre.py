850a7a41ba6978e5bfec702802898e952ac6d52d
Izayoii <rakhisahmat3600@gmail.com>
2026-04-14 14:24:03 +0200

    feat book class

--- EXO-3/livre.py
+++ EXO-3/livre.py
@@ -13,4 +13,24 @@ class Livre:
            print(f"Auteur: {self.auteur}")
            print(f"ISBN: {self.isbn}")
            print(f"Nombre de pages: {self.nb_pages}")
-           print(f"Disponible: {self.disponible}")
+           print(f"Disponible: {self.disponible}") 
+        
+        
+        def emprunter(self, nom_emprunteur):
+            if self.disponible:
+                self.disponible = False
+                self.emprunteur = nom_emprunteur
+                print(f"Le livre '{self.titre}' a été emprunté par {nom_emprunteur}")
+            else:
+                print(f"Le livre '{self.titre}' n'est pas disponible")
+                
+        def rendre(self):
+            if not self.disponible:
+                    self.disponible = True
+                    self.emprunteur = None
+                    print(f"Le livre '{self.titre}' a été rendu")
+            else:
+                    print(f"Le livre '{self.titre}' n'a pas été emprunté")
+                
+        def temps_lecture_estime(self):
+                    return self.nb_pages
