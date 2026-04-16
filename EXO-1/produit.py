618e29ae1c8d14aa8c650e4ab48bc135feb279ac
Izayoii <rakhisahmat3600@gmail.com>
2026-04-16 10:52:03 +0200

    feat creat product class

---
+++ EXO-1/produit.py
@@ -0,0 +1,26 @@
+class Produit:
+    tva = 20
+
+    def __init__(self, reference, nom, prix_ht):
+        self._reference = reference
+        self._nom       = nom
+        self._prix_ht   = prix_ht
+
+    def __str__(self):
+        return f"{self._nom} ({self._reference}) - {self._prix_ht}€ HT"
+
+    def __repr__(self):
+        return f"Produit('{self._reference}', '{self._nom}', {self._prix_ht})"
+
+    def __eq__(self, other):
+        if not isinstance(other, Produit): return False
+        return self._reference == other._reference
+
+    def __hash__(self):
+        return hash(self._reference)
+
+    def __lt__(self, other):
+        return self._prix_ht < other._prix_ht
+
+
+                                                                                                                                                                
